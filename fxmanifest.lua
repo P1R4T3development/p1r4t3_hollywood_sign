e99e9c51cfe36b9be65c6fa798a1f250ff988be6
P1R4T3_Developpement <124460873+P1R4T3development@users.noreply.github.com>
2023-06-03 06:30:06 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+--------------------------------------------------------
+--                                                    --
+--               credits: x_P1R4T3_x                  --
+--                                                    --
+--------------------------------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+description 'P1R4T3_CORE'
+author 'P1R4T3'
+this_is_a_map "yes"
